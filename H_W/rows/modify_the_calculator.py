11233e40fdf20b012568370a9f75e5fc252c15d6
melmanss <cfhfyjdbx@gmail.com>
2025-12-19 17:29:02 +0200

    new commit

---
+++ H_W/rows/modify_the_calculator.py
@@ -0,0 +1,44 @@
+def calculator(num1, operation, num2):
+    operation_map = {
+        '+': lambda a, b: a + b,
+        '-': lambda a, b: a - b,
+        '*': lambda a, b: a * b,
+        '/': lambda a, b: a / b,
+    }
+
+    if operation not in operation_map:
+        return "ПОМИЛКА: Неправильна операція."
+
+    if operation == '/' and num2 == 0:
+        return "ПОМИЛКА: Ділення на нуль неможливе."
+
+    try:
+        return operation_map[operation](num1, num2)
+    except Exception as e:
+        return f"ПОМИЛКА: Сталася невідома помилка: {e}"
+
+
+print("Калькулятор")
+
+while True:
+    try:
+        input_num1 = float(input("\nВведіть перше число: "))
+        input_operation = input("Введіть операцію "
+                                "(+, -, *, /): ").strip()
+        input_num2 = float(input("Введіть друге число: "))
+
+        final_result = calculator(input_num1, input_operation, input_num2)
+
+        if isinstance(final_result, str) and final_result.startswith("ПОМИЛКА"):
+            print(final_result)
+        else:
+            print(f"Результат: {input_num1} {input_operation} "
+                  f"{input_num2} = {final_result}")
+
+    except ValueError:
+        print("ПОМИЛКА: Введено некоректне число.")
+
+    choice = input("\nБажаєте продовжити? (y/yes): ").lower().strip()
+    if choice not in ['y', 'yes']:
+        print("Завершення роботи.")
+        break
