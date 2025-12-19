76c009626ecf41c640eb63d6b6d1025450e03273
melmanss <cfhfyjdbx@gmail.com>
2025-12-19 17:09:57 +0200

    update

--- H_W/listsandloops/list_generator.py
+++ H_W/listsandloops/list_generator.py
@@ -8,6 +8,7 @@ def select_three_elements(data: list) -> list:
         data[-2]
     ]
 
+
 test_cases = [
     [1, 2, 3, 4, 5, 6, 7, 9],
     [1, 1, 2, 1],
@@ -17,4 +18,5 @@ test_cases = [
 print("Результати")
 for original_list in test_cases:
     final_list = select_three_elements(original_list)
+
     print(f"{str(original_list):30}{final_list}")
