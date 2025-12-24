788ac2d79d12a6c68193a1a865d161070e3305c7
melmanss <cfhfyjdbx@gmail.com>
2025-12-24 16:09:35 +0200

    upd

--- H_W/rows/shift_name.py
+++ H_W/rows/shift_name.py
@@ -2,10 +2,22 @@ import string
 import keyword
 
 
-def is_valid_variable_name(name):
+def is_valid_variable_name(name: str) -> bool:
+    """
+    Перевіряє, чи може цей рядок бути ім'ям змінної, згідно зі специфічними правилами.
+
+    Правила включають: заборона починати з цифри,
+    відсутність великих літер, пробілів та знаків пунктуації (крім підкреслення),
+    а також обмеження на кількість знаків підкреслення.
+
+    Args:
+        name (str): Рядок, який потрібно перевірити.
+
+    Returns:
+        bool: True, якщо таке ім'я змінної допустимо, або False - якщо ні.
+    """
     if name in keyword.kwlist:
         return False
-
 
     if name[0].isdigit():
         return False
@@ -23,6 +35,7 @@ def is_valid_variable_name(name):
         return False
 
     return True
+
 
 user_input = input("Введіть ім'я змінної: ")
 print(is_valid_variable_name(user_input))
