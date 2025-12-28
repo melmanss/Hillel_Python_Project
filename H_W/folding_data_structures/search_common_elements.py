2ebdfeb1143ed4ecd84bad4931b3a848f11b50d2
melmanss <cfhfyjdbx@gmail.com>
2025-12-28 21:16:49 +0200

    upd

--- H_W/folding_data_structures/search_common_elements.py
+++ H_W/folding_data_structures/search_common_elements.py
@@ -1,15 +1,16 @@
+"""
+Модуль для пошуку спільних елементів кратних 3 та 5.
+"""
+
+
 def common_elements() -> set[int]:
     """
-    Генерує два списки чисел у діапазоні до 100: кратні 3 та кратні 5.
-    Знаходить спільні елементи для обох списків.
+        Знаходить числа до 100, які одночасно кратні 3 та 5.
 
-    Returns:
-        set[int]: Множина чисел, які є спільними кратними.
+        Returns:
+            set[int]: Множина чисел, які є спільними кратними.
     """
-    multiples_3: set[int] = {i for i in range(0, 100, 3)}
-    multiples_5: set[int] = {i for i in range(0, 100, 5)}
-
-    return multiples_3 & multiples_5
+    return set(range(0, 100, 3)) & set(range(0, 100, 5))
 
 
 result: set[int] = common_elements()
