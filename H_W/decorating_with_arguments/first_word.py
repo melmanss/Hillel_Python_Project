56404c1368acb07e866bd8e42dd8035039c06c57
melmanss <cfhfyjdbx@gmail.com>
2026-01-09 14:57:47 +0200

    new

---
+++ H_W/decorating_with_arguments/first_word.py
@@ -0,0 +1,26 @@
+import re
+from typing import Optional
+
+
+def first_word(text: str) -> str:
+    """
+    Знайти перше слово у переданому рядку.
+
+    Слово може містити літери та апостроф. Знаки пунктуації та пробіли
+    на початку рядка ігноруються.
+    """
+    pattern: str = r"[a-zA-Zа-яА'ʼ]+"
+    match: Optional[re.Match] = re.search(pattern, text)
+
+    if match:
+        return match.group(0)
+    return ""
+
+
+print(f"{first_word('Hello world')}")
+print(f"{first_word(' a word ')}")
+print(f"{first_word('... and so on ...')}")
+print(f"{first_word("don't touch it")}")
+print(f"{first_word('greetings, friends')}")
+print(f"{first_word('...and... ')}")
+print(f"{first_word('only one')}")
